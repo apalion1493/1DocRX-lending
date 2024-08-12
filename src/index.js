aceb47406c5d2534a7d6cc0643d194333a832d3e
Alexandr Petrovich <apalion97@gmail.com>
2024-08-12 11:48:25 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './styles.css';
+
+console.log('TailwindCSS + Webpack starter template');
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader', // Обработка CSS с помощью PostCSS
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css', // Название выходного CSS файла
+        }),
+        new ImageMinimizerPlugin({
+            minimizer: {
+                implementation: ImageMinimizerPlugin.imageminGenerate,
+                options: {
+                    plugins: [
+                        ['imagemin-mozjpeg', { quality: 75 }],
+                        ['imagemin-pngquant', { quality: [0.65, 0.90], speed: 4 }],
+                    ],
+                },
+            },
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+        liveReload: true,
+        watchFiles: ['src/**/*.html', 'src/**/*.css', 'src/**/*.js'],
+    },
+    mode: 'development',
+    optimization: {
+        minimize: true,
+        minimizer: [
+            '...',
+            new ImageMinimizerPlugin({
+                minimizer: {
+                    implementation: ImageMinimizerPlugin.imageminGenerate,
+                    options: {
+                        plugins: [
+                            ['imagemin-mozjpeg', { quality: 75 }],
+                            ['imagemin-pngquant', { quality: [0.65, 0.90], speed: 4 }],
+                        ],
+                    },
+                },
+            }),
+        ],
+    },
+};

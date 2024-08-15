428d31f72df93c63e39914b387d56bf495b43e5f
Alexandr Petrovich <apalion97@gmail.com>
2024-08-15 11:06:57 +0300

    Add Swiper for customer feedback section
    Integrated Swiper to create a sliding customer feedback section on the homepage. Updated styles and markup to support Swiper functionality, and added necessary Swiper configurations in JavaScript.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,26 @@ import Alpine from 'alpinejs';
 import collapse from '@alpinejs/collapse';
 import mask from '@alpinejs/mask';
 import focus from '@alpinejs/focus';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 window.Alpine = Alpine
 Alpine.plugin(collapse, mask, focus)
-
 Alpine.start()
+
+// init Swiper:
+const swiper = new Swiper('.mySwiper', {
+    slidesPerView: 3,
+    spaceBetween: 24,
+    loop: true,
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
